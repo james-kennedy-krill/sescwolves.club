bf973babfbb79afb5fe0162652aaa9077381f84a
James Kennedy Krill <me@jameskennedykrill.com>
2021-07-18 13:15:16 -0700

    feat: added auth0, still need to set it up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/index.css";
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <div className="container mx-auto">
+        <Component {...pageProps} />
+      </div>
+    </UserProvider>
+  );
 }
-export default MyApp
+export default MyApp;
