c496d119f36ed5bb25d5eac125e21b0268a6705b
James Kennedy Krill <me@jameskennedykrill.com>
2021-08-03 21:41:25 -0700

    fixing errors

--- components/Account.tsx
+++ components/Account.tsx
@@ -4,13 +4,17 @@ import { useUser } from "@auth0/nextjs-auth0";
 
 const Account = (): JSX.Element => {
   const { user, error, isLoading } = useUser();
-  console.log(user);
 
   return (
     <div>
       {user && !!user.picture && (
         <span className="rounded-full h-10 w-10 ml-5 flex items-center justify-center overflow-hidden">
-          <Image src={user.picture} alt="Profile Picture" />
+          <Image
+            src={user.picture}
+            alt="Profile Picture"
+            width={40}
+            height={40}
+          />
         </span>
       )}
     </div>
