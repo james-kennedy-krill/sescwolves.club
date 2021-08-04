1327003d8f4c9f8647fc5c7983eeadd393ad61e2
James Kennedy Krill <me@jameskennedykrill.com>
2021-08-04 07:24:24 -0700

    safelist for purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    options: {
+      safelist: [
+        "bg-red-500",
+        "bg-yellow-500",
+        "bg-green-500",
+        "bg-red-300",
+        "bg-yellow-300",
+        "bg-green-300",
+      ],
+    },
+  },
   darkMode: false,
   theme: {
     extend: {
