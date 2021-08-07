3a07212b3dc03b3e62ffadc1c3921a2e26ce5f15
James Kennedy Krill <me@jameskennedykrill.com>
2021-08-07 07:51:58 -0700

    updated schedule

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,7 +1,9 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Head from "next/head";
+import Image from "next/image";
 import Navbar from "../components/Navbar";
 import { hasRole } from "../components/utils";
+import mthoodchallenge from "../public/mthoodchallenge.jpg";
 
 const Schedule = withPageAuthRequired(() => {
   const { user, error, isLoading } = useUser();
@@ -20,7 +22,34 @@ const Schedule = withPageAuthRequired(() => {
           <h1 className="text-4xl font-bold text-center mb-2">
             2021-2022 Schedule
           </h1>
-          <p>Coming soon!</p>
+          <hr />
+          <div className="my-5 py-5">
+            <h2 className="text-3xl font-bold mb-4">Mt. Hood Challenge</h2>
+            <h3 className="text-2xl font-bold">Saturday, August 7th 2021</h3>
+            <p className="mb-3">
+              <strong>9:00am</strong>: @ Pacific FC Fuego{" "}
+              <span className="text-gray-500 italic">Field 2B</span>
+              <br /> (wear BLUE; bring WHITE)
+            </p>
+            <p className="mb-3">
+              <strong>6:00pm</strong>: vs. WUFC Gold{" "}
+              <span className="text-gray-500 italic">Field 2A</span>
+              <br /> (wear WHITE; bring BLUE)
+            </p>
+
+            <h3 className="text-2xl font-bold">Sunday, August 8th 2021</h3>
+            <p className="mb-3">
+              <strong>9:00am</strong>: vs Eastside Thorns Red{" "}
+              <span className="text-gray-500 italic">Field 2A</span>
+              <br /> (wear WHITE; bring BLUE)
+            </p>
+            <p className="mb-3">
+              <strong>1:30pm</strong>: vs. TBD{" "}
+              <span className="text-gray-500 italic">Field 2A</span>
+              <br /> (if necessary, championship game)
+            </p>
+          </div>
+          <Image src={mthoodchallenge} />
         </div>
       </main>
     </div>
