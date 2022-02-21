05ad171261a401fdb15e59ee384d4fc256625ca5
James Kennedy Krill <me@jameskennedykrill.com>
2022-02-21 10:06:49 -0800

    updated schedule view

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -4,7 +4,7 @@ import useSWR from "swr";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 import Navbar from "../components/Navbar";
 import { hasRole } from "../components/utils";
 import { Container } from "../styles/common-styles";
@@ -18,6 +18,7 @@ type Game = {
   homeOrAway: string;
   opponent: string;
   result: string;
+  notes?: string;
 };
 
 type Schedule = {
@@ -29,6 +30,10 @@ const Table = tw.table`w-full border-collapse`;
 const TableHeader = tw.th`font-bold text-xs md:text-lg text-left p-1 md:p-2`;
 const TableDateRow = tw.tr`text-sm md:text-base border-b-2 border-gray-300 bg-gray-100`;
 const TableData = tw.td`text-xs md:text-base pt-1 pb-2 px-1 md:px-2 text-left`;
+const TableDataNotes = styled(TableData)(() => [
+  tw`italic`,
+  tw`align-middle pt-0 text-sm`,
+]);
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -92,16 +97,30 @@ const Schedule = () => {
                         <TableData></TableData>
                       </TableDateRow>
                       {date.games.map((game: Game) => (
-                        <tr
-                          key={`${game.team}-${game.homeOrAway}-${game.opponent}`}
-                        >
-                          <TableData>{game.team}</TableData>
-                          <TableData>{game.time}</TableData>
-                          <TableData>{game.field}</TableData>
-                          <TableData>{game.homeOrAway}</TableData>
-                          <TableData>{game.opponent}</TableData>
-                          <TableData>{game.result}</TableData>
-                        </tr>
+                        <>
+                          <tr
+                            key={`${game.team}-${game.homeOrAway}-${game.opponent}`}
+                          >
+                            <TableData>{game.team}</TableData>
+                            <TableData>{game.time}</TableData>
+                            <TableData>{game.field}</TableData>
+                            <TableData>{game.homeOrAway}</TableData>
+                            <TableData>{game.opponent}</TableData>
+                            <TableData>{game.result}</TableData>
+                          </tr>
+                          {game.notes && (
+                            <tr>
+                              <TableDataNotes colSpan={6}>
+                                <p tw="flex items-center">
+                                  <span className="material-icons pr-2">
+                                    sports_soccer
+                                  </span>
+                                  {game.notes}
+                                </p>
+                              </TableDataNotes>
+                            </tr>
+                          )}
+                        </>
                       ))}
                     </>
                   ))}
